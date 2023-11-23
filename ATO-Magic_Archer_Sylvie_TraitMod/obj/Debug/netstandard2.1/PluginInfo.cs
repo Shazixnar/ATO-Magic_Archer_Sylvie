1045042157c4e848b868db6f22437db94f2ad2aa
Shazixnar <alexrl@qq.com>
2023-11-23 19:10:42 +0800

    initial commit

--- ATO-Magic_Archer_Sylvie_TraitMod/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace TraitMod
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "com.shazixnar.MagicArchertraits";
-        public const string PLUGIN_NAME = "TraitMod";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
