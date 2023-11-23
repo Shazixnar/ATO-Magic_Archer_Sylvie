3135c5dc9cc5f784b9fc784cba443f7bd0cffac6
Shazixnar <alexrl@qq.com>
2023-11-23 20:17:06 +0800

    re updated version 2.0.3

---
+++ ATO-Magic_Archer_Sylvie_TraitMod/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace TraitMod
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "com.shazixnar.MagicArchertraits";
+        public const string PLUGIN_NAME = "TraitMod";
+        public const string PLUGIN_VERSION = "2.0.3";
+    }
+}
