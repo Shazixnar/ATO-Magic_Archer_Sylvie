eb45152cb488d59e03ea4bfad0f220360c2d2852
stiffmeds <6158160+stiffmeds@users.noreply.github.com>
2023-12-17 12:17:16 +0800

    Delete obj

--- ATO-Magic_Archer_Sylvie_TraitMod/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MagicArcherSylvie
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "com.shazixnar.MagicArcherTraits";
-        public const string PLUGIN_NAME = "Magic Archer Sylvie";
-        public const string PLUGIN_VERSION = "2.0.5";
-    }
-}
